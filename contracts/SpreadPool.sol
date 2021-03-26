91bd38bf605e298d9cf7a9c104e10bc9a022282e
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-03-26 18:17:10 +0000

    Add more params

--- contracts/SpreadPool.sol
+++ contracts/SpreadPool.sol
@@ -19,6 +19,7 @@ contract SpreadPool is ERC20, Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
     using SafeMath for int24;
     using SafeMath for uint256;
+    using SafeMath for int56;
 
     IERC20 public token0;
     IERC20 public token1;
@@ -26,7 +27,9 @@ contract SpreadPool is ERC20, Ownable, ReentrancyGuard {
 
     int24 public baseWidth;
     int24 public rebalanceWidth;
-    uint256 public twapDuration;
+    uint32 public twapDuration;
+    uint256 withdrawFee;
+    uint256 performanceFee;
     bool public depositPaused;
     bool public withdrawPaused;
     bool public finalized;
@@ -65,22 +68,32 @@ contract SpreadPool is ERC20, Ownable, ReentrancyGuard {
         amount0 = totalBalance0().mul(amount).div(_totalSupply);
         amount1 = totalBalance1().mul(amount).div(_totalSupply);
 
+        amount0 = amount0.sub(amount0.mul(withdrawFee).div(1e6);
+        amount1 = amount1.sub(amount1.mul(withdrawFee).div(1e6);
+
         burn(msg.sender, amount);
         token0.safeTransfer(msg.sender, amount0);
         token1.safeTransfer(msg.sender, amount1);
     }
 
-    function sync() external onlyOwner {
+    function update() external onlyOwner {
         // TODO remove liquidity
 
         // TODO add back base position
 
 
         // TODO add rebalancing position
+
+        // TODO collect fees
     }
 
-    function getPrice() public view returns (uint256) {
-        // TODO get twap from oracle
+    /**
+     * Get twap price from oracle in ticks
+     */
+    function getPriceInTicks() public view returns (int56) {
+        uint32 secondsAgos[2] = [twapDuration, 0];
+        (int56[] tickCumulatives, ) = uniswapV3Pool.observe(secondsAgos);
+        return tickCumulatives[1].sub(tickCumulatives[0]);
     }
 
     function totalBalance0() public view returns (uint256) {
@@ -96,32 +109,45 @@ contract SpreadPool is ERC20, Ownable, ReentrancyGuard {
     /**
      * @param _baseWidth Target width of base liquidity in ticks or basis points
      */
-    function setBaseWidth(int24 _baseWidth) external nonReentrant onlyOwner {
+    function setBaseWidth(int24 _baseWidth) external onlyOwner {
         baseWidth = _baseWidth;
     }
 
     /**
      * @param _rebalanceWidth Target width of rebalance liquidity in ticks or basis points
      */
-    function setRebalanceWidth(int24 _rebalanceWidth) external nonReentrant onlyOwner {
+    function setRebalanceWidth(int24 _rebalanceWidth) external onlyOwner {
         rebalanceWidth = _rebalanceWidth;
     }
 
     /**
      * @param _twapDuration Duration of twap price fetched from oracle in seconds
      */
-    function setTwapDuration(int24 _twapDuration) external nonReentrant onlyOwner {
+    function setTwapDuration(uint32 _twapDuration) external onlyOwner {
         twapDuration = _twapDuration;
     }
 
+    /**
+     * @param _withdrawFee Fee charged on withdrawal multiplied by 1e6
+     */
+    function setWithdrawFee(bool _withdrawFee) external onlyOwner {
+        withdrawFee = _withdrawFee;
+    }
+
+    /**
+     * @param _performanceFee Fee charged when collecting Uniswap fees
+     */
+    function setPerformanceFee(bool _performanceFee) external onlyOwner {
+        performanceFee = _performanceFee;
+    }
 
     // TODO think more about what powers are needed
 
-    function setDepositPaused(bool paused) external nonReentrant onlyOwner {
+    function setDepositPaused(bool paused) external onlyOwner {
         depositPaused = paused;
     }
 
-    function setWithdrawPaused(bool paused) external nonReentrant onlyOwner {
+    function setWithdrawPaused(bool paused) external onlyOwner {
         withdrawPaused = paused;
     }
 
@@ -148,16 +174,18 @@ contract SpreadPool is ERC20, Ownable, ReentrancyGuard {
 
 /**
 
-    struct MintParams {
-        address token0;
-        address token1;
-        uint24 fee;
-        int24 tickLower;
-        int24 tickUpper;
-        uint128 amount;
-        uint256 amount0Max;
-        uint256 amount1Max;
-        address recipient;
-        uint256 deadline;
-    }
+https://docs.uniswap.org/
+
+struct MintParams {
+    address token0;
+    address token1;
+    uint24 fee;
+    int24 tickLower;
+    int24 tickUpper;
+    uint128 amount;
+    uint256 amount0Max;
+    uint256 amount1Max;
+    address recipient;
+    uint256 deadline;
+}
 */
