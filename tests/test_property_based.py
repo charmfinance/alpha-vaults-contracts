fddebced85235b2cfb230296309902e590324560
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-05-12 17:31:50 +0100

    Add more property-based tests to check for manipulation attacks

--- tests/test_property_based.py
+++ tests/test_property_based.py
@@ -37,15 +37,170 @@ def test_rebalance_uses_up_all_balances(
     vault, pool, router, gov, user, tokens, amount0Desired, amount1Desired, buy, qty
 ):
 
-    # Deposit, move price and rebalance to simulate existing activity
+    # Simulate random deposit and random price move
     vault.deposit(amount0Desired, amount1Desired, 0, 0, user, {"from": user})
     vault.rebalance({"from": user})
-
     router.swap(pool, buy, qty, {"from": user})
-    vault.setMaxTwapDeviation(1 << 22, {"from": gov})  # ignore twap deviation
+
+    # Ignore TWAP deviation
+    vault.setMaxTwapDeviation(1 << 22, {"from": gov})
 
     vault.rebalance({"from": user})
 
-    # Check balances is roughly zero
+    # Check leftover balances is low
     assert tokens[0].balanceOf(vault) < 10000
     assert tokens[1].balanceOf(vault) < 10000
+
+
+@given(amount0Desired=strategy("uint256", min_value=1e8, max_value=1e18))
+@given(amount1Desired=strategy("uint256", min_value=1e8, max_value=1e18))
+@given(buy=strategy("bool"))
+@given(qty=strategy("uint256", min_value=1e3, max_value=1e18))
+@settings(max_examples=MAX_EXAMPLES)
+def test_cannot_make_instant_profit_from_deposit_then_withdraw(
+    vault, pool, router, gov, user, tokens, amount0Desired, amount1Desired, buy, qty
+):
+
+    # Simulate deposit and random price move
+    vault.deposit(1e16, 1e18, 0, 0, user, {"from": user})
+    vault.rebalance({"from": user})
+    router.swap(pool, buy, qty, {"from": user})
+
+    # Deposit
+    tx = vault.deposit(amount0Desired, amount1Desired, 0, 0, user, {"from": user})
+    shares, amount0Deposit, amount1Deposit = tx.return_value
+
+    # Withdraw all
+    tx = vault.withdraw(shares, 0, 0, user, {"from": user})
+    amount0Withdraw, amount1Withdraw = tx.return_value
+
+    # Check did not make a profit
+    assert amount0Deposit >= amount0Withdraw
+    assert amount1Deposit >= amount1Withdraw
+
+    # Check amounts are roughly equal
+    assert approx(amount0Deposit, abs=3) == amount0Withdraw
+    assert approx(amount1Deposit, abs=3) == amount1Withdraw
+
+
+@given(amount0Desired=strategy("uint256", min_value=1e8, max_value=1e18))
+@given(amount1Desired=strategy("uint256", min_value=1e8, max_value=1e18))
+@given(buy=strategy("bool"))
+@given(buy2=strategy("bool"))
+@given(qty=strategy("uint256", min_value=1e3, max_value=1e18))
+@given(qty2=strategy("uint256", min_value=1e3, max_value=1e18))
+@settings(max_examples=MAX_EXAMPLES)
+def test_cannot_make_instant_profit_from_manipulated_deposit(
+    vault, pool, router, gov, user, tokens, amount0Desired, amount1Desired, buy, qty, buy2, qty2
+):
+
+    # Simulate deposit and random price move
+    vault.deposit(1e16, 1e18, 0, 0, user, {"from": user})
+    vault.rebalance({"from": user})
+    router.swap(pool, buy, qty, {"from": user})
+
+    # Store initial balances
+    balance0 = tokens[0].balanceOf(user)
+    balance1 = tokens[1].balanceOf(user)
+
+    # Manipulate
+    router.swap(pool, buy2, qty2, {"from": user})
+
+    # Deposit
+    tx = vault.deposit(amount0Desired, amount1Desired, 0, 0, user, {"from": user})
+    shares, _, _ = tx.return_value
+
+    # Manipulate price back
+    router.swap(pool, not buy2, -qty2, {"from": user})
+
+    # Withdraw all
+    vault.withdraw(shares, 0, 0, user, {"from": user})
+    price = 1.0001 ** pool.slot0()[1]
+
+    balance0After = tokens[0].balanceOf(user)
+    balance1After = tokens[1].balanceOf(user)
+
+    # Check did not make a profit
+    value = balance0 * price + balance1
+    valueAfter = balance0After * price + balance1After
+    assert value >= valueAfter
+
+
+@given(amount0Desired=strategy("uint256", min_value=1e8, max_value=1e18))
+@given(amount1Desired=strategy("uint256", min_value=1e8, max_value=1e18))
+@given(buy=strategy("bool"))
+@given(buy2=strategy("bool"))
+@given(qty=strategy("uint256", min_value=1e3, max_value=1e18))
+@given(qty2=strategy("uint256", min_value=1e3, max_value=1e18))
+@settings(max_examples=MAX_EXAMPLES)
+def test_cannot_make_instant_profit_from_manipulated_withdraw(
+    vault, pool, router, gov, user, tokens, amount0Desired, amount1Desired, buy, qty, buy2, qty2
+):
+
+    # Simulate deposit and random price move
+    vault.deposit(1e16, 1e18, 0, 0, user, {"from": user})
+    vault.rebalance({"from": user})
+    router.swap(pool, buy, qty, {"from": user})
+
+    # Store initial balances
+    balance0 = tokens[0].balanceOf(user)
+    balance1 = tokens[1].balanceOf(user)
+
+    # Deposit
+    tx = vault.deposit(amount0Desired, amount1Desired, 0, 0, user, {"from": user})
+    shares, _, _ = tx.return_value
+    price = 1.0001 ** pool.slot0()[1]
+
+    # Manipulate
+    router.swap(pool, buy2, qty2, {"from": user})
+
+    # Withdraw all
+    vault.withdraw(shares, 0, 0, user, {"from": user})
+    priceAfter = 1.0001 ** pool.slot0()[1]
+
+    balance0After = tokens[0].balanceOf(user)
+    balance1After = tokens[1].balanceOf(user)
+
+    # Check did not make a profit
+    value = balance0 * price + balance1
+    valueAfter = balance0After * price + balance1After
+    assert value >= valueAfter
+
+
+@given(amount0Desired=strategy("uint256", min_value=1e8, max_value=1e18))
+@given(amount1Desired=strategy("uint256", min_value=1e8, max_value=1e18))
+@given(buy=strategy("bool"))
+@given(buy2=strategy("bool"))
+@given(qty=strategy("uint256", min_value=1e3, max_value=1e18))
+@given(qty2=strategy("uint256", min_value=1e3, max_value=1e18))
+@settings(max_examples=MAX_EXAMPLES)
+def test_cannot_make_instant_profit_around_rebalance(
+    vault, pool, router, gov, user, tokens, amount0Desired, amount1Desired, buy, qty, buy2, qty2
+):
+
+    # Simulate deposit and random price move
+    vault.deposit(1e16, 1e18, 0, 0, user, {"from": user})
+    vault.rebalance({"from": user})
+    router.swap(pool, buy, qty, {"from": user})
+
+    # Store totals before
+    total0, total1 = vault.getTotalAmounts()
+
+    # Deposit
+    tx = vault.deposit(amount0Desired, amount1Desired, 0, 0, user, {"from": user})
+    shares, amount0Deposit, amount1Deposit = tx.return_value
+
+    # Rebalance
+    vault.rebalance({"from": user})
+
+    # Withdraw all
+    tx = vault.withdraw(shares, 0, 0, user, {"from": user})
+    amount0Withdraw, amount1Withdraw = tx.return_value
+    total0After, total1After = vault.getTotalAmounts()
+
+    assert not (amount0Deposit < amount0Withdraw and amount1Deposit <= amount1Withdraw)
+    assert not (amount0Deposit <= amount0Withdraw and amount1Deposit < amount1Withdraw)
+
+    assert total0 <= total0After + 1
+    assert total1 <= total1After + 1
+
