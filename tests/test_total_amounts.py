d0f831c92caaaff4a638d50ba79852872a2724cc
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-05-19 12:21:15 +0100

    Add test that total amounts includes accrued fees

---
+++ tests/test_total_amounts.py
@@ -0,0 +1,47 @@
+import pytest
+from pytest import approx
+
+
+@pytest.mark.parametrize(
+    "amount0Desired,amount1Desired",
+    [[1e18, 1e4], [1e4, 1e18], [1e18, 1e18]],
+)
+def test_total_amounts_includes_fees(
+    vaultAfterPriceMove,
+    pool,
+    router,
+    tokens,
+    getPositions,
+    gov,
+    user,
+    recipient,
+    amount0Desired,
+    amount1Desired,
+):
+    vault = vaultAfterPriceMove
+
+    # First deposit
+    tx = vault.deposit(amount0Desired, amount1Desired, 0, 0, user, {"from": user})
+    shares, _, _ = tx.return_value
+
+    total0, total1 = vault.getTotalAmounts()
+
+    # Generate fees
+    router.swap(pool, True, 1e16, {"from": gov})
+    router.swap(pool, False, -1e16 * 0.997, {"from": gov})
+
+    total0After, total1After = vault.getTotalAmounts()
+    assert approx(total0After) == total0
+    assert approx(total1After) == total1
+    assert total0After < total0 or total1After < total1
+    assert total0After > total0 or total1After > total1
+
+    # Poke pool
+    vault.withdraw(1, 0, 0, user, {"from": user})
+    total0After, total1After = vault.getTotalAmounts()
+    assert approx(total0After, rel=1e-2) == total0
+    assert approx(total1After, rel=1e-2) == total1
+
+    # Check total amounts grew due to fees
+    assert total0After > total0
+    assert total1After > total1
