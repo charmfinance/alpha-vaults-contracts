33ca9fe92b173808f4c0e471bc30d0e2628cc912
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-07-02 12:08:22 +0100

    Add passive strategy

--- tests/test_rebalance.py
+++ tests/test_rebalance.py
@@ -7,7 +7,7 @@ from conftest import computePositionKey
 
 @pytest.mark.parametrize("buy", [False, True])
 @pytest.mark.parametrize("big", [False, True])
-def test_rebalance(
+def test_strategy_rebalance(
     vault, strategy, pool, tokens, router, getPositions, gov, user, keeper, buy, big
 ):
     # Mint some liquidity
@@ -26,7 +26,6 @@ def test_rebalance(
     # Store totals
     total0, total1 = vault.getTotalAmounts()
     totalSupply = vault.totalSupply()
-    govShares = vault.balanceOf(gov)
 
     # Rebalance
     tx = strategy.rebalance({"from": keeper})
@@ -57,15 +56,15 @@ def test_rebalance(
     assert strategy.lastTick() == tick
     assert strategy.getTick() == tick
 
-    base, rebalance = getPositions(vault)
+    base, limit = getPositions(vault)
 
     if big:
-        # If order is too big, all tokens go to rebalance order
+        # If order is too big, all tokens go to limit order
         assert base[0] == 0
-        assert rebalance[0] > 0
+        assert limit[0] > 0
     else:
         assert base[0] > 0
-        assert rebalance[0] > 0
+        assert limit[0] > 0
 
     # Check no tokens left unused. Only small amount left due to rounding
     assert tokens[0].balanceOf(vault) - vault.accruedProtocolFees0() < 1000
@@ -102,7 +101,7 @@ def test_rebalance(
 
 @pytest.mark.parametrize("buy", [False, True])
 def test_rebalance_twap_check(
-    vault, strategy, pool, tokens, router, getPositions, gov, user, keeper, buy
+    vault, strategy, pool, tokens, router, gov, user, keeper, buy
 ):
 
     # Reduce max deviation
@@ -145,6 +144,90 @@ def test_can_rebalance_when_vault_empty(
 
     assert strategy.lastRebalance() == tx.timestamp
     assert strategy.lastTick() == tick
+
+
+@pytest.mark.parametrize("bid", [False, True])
+def test_rebalance(
+    vault, strategy, pool, tokens, router, getPositions, gov, user, keeper, bid
+):
+    # Mint some liquidity
+    vault.deposit(1e16, 1e18, 0, 0, user, {"from": user})
+
+    # Store old state
+    baseLower, baseUpper = vault.baseLower(), vault.baseUpper()
+    limitLower, limitUpper = vault.limitLower(), vault.limitUpper()
+    total0, total1 = vault.getTotalAmounts()
+    totalSupply = vault.totalSupply()
+    tick = pool.slot0()[1]
+    assert 42000 < tick < 48000
+
+    # Rebalance
+    if bid:
+        tx = vault.rebalance(
+            0, 0, 42000, 54000, -120000, -60000, 60000, 120000, {"from": strategy}
+        )
+    else:
+        tx = vault.rebalance(
+            0, 0, 36000, 48000, -120000, -60000, 60000, 120000, {"from": strategy}
+        )
+
+    # Check old positions are empty
+    liquidity, _, _, owed0, owed1 = pool.positions(
+        computePositionKey(vault, baseLower, baseUpper)
+    )
+    assert liquidity == owed0 == owed1 == 0
+    liquidity, _, _, owed0, owed1 = pool.positions(
+        computePositionKey(vault, limitLower, limitUpper)
+    )
+    assert liquidity == owed0 == owed1 == 0
+
+    # Check ranges are set correctly
+    if bid:
+        assert vault.baseLower() == 42000
+        assert vault.baseUpper() == 54000
+        assert vault.limitLower() == -120000
+        assert vault.limitUpper() == -60000
+    else:
+        assert vault.baseLower() == 36000
+        assert vault.baseUpper() == 48000
+        assert vault.limitLower() == 60000
+        assert vault.limitUpper() == 120000
+
+    base, limit = getPositions(vault)
+    assert base[0] > 0
+    assert limit[0] > 0
+
+    # Check no tokens left unused. Only small amount left due to rounding
+    assert tokens[0].balanceOf(vault) - vault.accruedProtocolFees0() < 1000
+    assert tokens[1].balanceOf(vault) - vault.accruedProtocolFees1() < 1000
+
+    # Check event
+    total0After, total1After = vault.getTotalAmounts()
+    (ev,) = tx.events["Snapshot"]
+    assert ev["tick"] == tick
+    assert approx(ev["totalAmount0"]) == total0After
+    assert approx(ev["totalAmount1"]) == total1After
+    assert ev["totalSupply"] == vault.totalSupply()
+
+    (ev1, ev2) = tx.events["CollectFees"]
+    dtotal0 = total0After - total0 + ev1["feesToProtocol0"] + ev2["feesToProtocol0"]
+    dtotal1 = total1After - total1 + ev1["feesToProtocol1"] + ev2["feesToProtocol1"]
+    assert (
+        approx(ev1["feesToVault0"] + ev2["feesToVault0"], rel=1e-6, abs=1)
+        == dtotal0 * 0.99
+    )
+    assert (
+        approx(ev1["feesToProtocol0"] + ev2["feesToProtocol0"], rel=1e-6, abs=1)
+        == dtotal0 * 0.01
+    )
+    assert (
+        approx(ev1["feesToVault1"] + ev2["feesToVault1"], rel=1e-6, abs=1)
+        == dtotal1 * 0.99
+    )
+    assert (
+        approx(ev1["feesToProtocol1"] + ev2["feesToProtocol1"], rel=1e-6, abs=1)
+        == dtotal1 * 0.01
+    )
 
 
 def test_rebalance_checks(vault, strategy, pool, gov, user, keeper):
